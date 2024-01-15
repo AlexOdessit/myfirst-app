9e608ab8439374cd6bcf51dced3ad165aa5d43b8
Alex <alexgofresh@gmail.com>
2024-01-15 16:31:03 +0100

    UserCard added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
-import Greeting from './components/Greetings';
+// import Greeting from './components/Greetings';
 import './App.css';
 import TextElement from './components/TextElement';
 import Phone from './components/Phone';
-
-const user1 = {
-  name: 'User',
-  lastName: 'Userenko',
-};
-
-const user2 = {
-  name: 'Test',
-  lastName: 'Testovich',
-};
-
-const user3 = {
-  name: 'John',
-  lastName: 'Doe',
-};
+import UserCard, { user1, user2 } from './components/UserCard';
 
 function App() {
   return (
     <>
       <Header />
       <MainContent />
-      <Greeting user={user1} />
-      <Greeting user={user2} />
-      <Greeting user={user3} />
+
       <TextElement text='Long lorem ipsum text' tooltip='this is title' />
       <TextElement text='Other text' tooltip='other title' />
       <Phone color='black' price={15999.99} />
+      <UserCard user={user1} />
+      <UserCard user={user2} />
     </>
   );
 }
--- src/components/Phone/index.jsx
+++ src/components/Phone/index.jsx
@@ -1,26 +1,114 @@
 import React from 'react';
+import './style.css';
 
 class Phone extends React.Component {
   constructor(props) {
     super(props);
     // стан існує тільки у класових компонентів
     this.state = {
-      phoneState: 'off',
+      isOn: false,
+      isPhoneShown: true,
+      phones: [
+        {
+          id: 0,
+          color: 'black',
+          price: 1599,
+        },
+        {
+          id: 0,
+          color: 'light',
+          price: 1099,
+        },
+        {
+          id: 0,
+          color: 'gold',
+          price: 20000,
+        },
+      ],
     };
+
+    // прикрутили this
+    // this.handleToggleOn = this.handleToggleOn.bind(this);
   }
+
+  // handleToggleOn() {
+  //   // setState - метод який приймає об'єкт і зливає його з поточним станом
+  //   // після чого реакт робить повторний рендер
+  //   const stateChanges = {
+  //     isOn: true,
+  //   };
+  //   this.setState(stateChanges);
+  //   /*
+  //     доробити телефону кнопку вимикання,
+  //     при якій його стан має змінитися на off
+  //   */
+  // }
+
+  // this береться автоматично
+  // handleToggleOff = () => {
+  //   this.setState({
+  //     isOn: false,
+  //   });
+  // };
+
+  handleToggle = () => {
+    const { isOn } = this.state;
+    this.setState({
+      // isOn: !this.state.isOn
+      isOn: !isOn,
+    });
+  };
 
   render() {
     const { color, price } = this.props;
-    const { phoneState } = this.state;
+    const { isOn } = this.state;
+
+    // let phoneStatus;
+
+    // if(phoneState === 'on') {
+    //   phoneStatus = <PhoneOnText />;
+    // } else {
+    //   phoneStatus = <PhoneOffText/>;
+    // }
+
+    const phoneStatus = isOn ? <PhoneOnText /> : <PhoneOffText />;
+
+    const containerClass = `phoneContainer ${isOn ? 'on' : 'off'}`;
+
     return (
-      <div>
+      <div className={containerClass}>
         <p>Phone is {color}</p>
         <p>Phone price is {price}</p>
-        <p>Phone is currently {phoneState}</p>
-        <button>Switch on</button>
+        {phoneStatus} handleToggleOn(){' '}
+        {
+          // setState - метод який приймає об'єкт і зливає його з поточним станом
+          // після чого реакт робить повторний рендер
+          // const stateChanges = {
+          //   isOn: true,
+          // };
+          // this.setState(stateChanges   const phoneStatus = isOn ? <PhoneOnText /> : <PhoneOffText />;);
+          // /*
+          // доробити телефону кнопку вимикання,
+          // при якій його стан має змінитися на off
+        }
+        <button onClick={this.handleToggle}>
+          Switch {isOn ? 'on' : 'off'}
+        </button>
       </div>
     );
   }
+}
+
+/*
+  умовний рендерінг полягає у відмальуванні певних компонентів / елементів / тексту
+  за певної умови
+*/
+function PhoneOnText() {
+  return <p>Phone is currently on</p>;
+}
+
+function PhoneOffText() {
+  return <p>Phone is currently off</p>;
 }
 
 export default Phone;
